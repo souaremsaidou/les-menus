47675a3b2581dcf397b6ccf02443bb076ddc23ba
Saidou <57505334+souaremsaidou@users.noreply.github.com>
2020-12-18 10:37:32 +0100

    TFT FeatherWing

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,66 @@
 #include <Arduino.h>
+
+/********************
+Dec.2020 SOUARE Saidou - souaremsaidou@gmail.com
+
+menu with with Adafruit_HX8357 driver https://github.com/adafruit/Adafruit_HX8357_Library
+TODO: output: not sure yet... ;)
+TODO: input: Serial + encoder
+
+TODO: alternative encoder (clickEncoder) uses:
+TODO: https://github.com/0xPIT/encoder
+TODO: https://github.com/PaulStoffregen/TimerOne
+***/
+
+//removed as extra libs are not included into travis
+// #define USE_CLICK_ENCODER
+
+#include <rotary.h>
+
+
+#include <SPI.h>
 #include <Wire.h>
-/***************************************************************************************************************************************
-LILYGO TTGO T-DISPLAY DEMO
-Created at August 29th, 2019 by Jeroen Maathuis (j [dot] maathuis [at] gmail [dot] com) to support the LilyGo TTGO T-display
-
-Based on:
-- ArduinoMenu TFT_eSPI example on https://github.com/neu-rah/ArduinoMenu/blob/master/examples/adafruitGfx/eTFT/TFT_eSPI/TFT_eSPI.ino.
-- TTGO T-display example on https://github.com/Xinyuan-LilyGO/TTGO-T-Display/blob/master/TTGO-T-Display.ino.
-- Several Button2 examples on https://github.com/LennartHennigs/Button2/tree/master/examples
-
-External needed library's:
-- TFT_eSPI
-- Buttons2
-
-Additional needed files:
-- bmp.h (should be found at the same place as this sketch file)
-
-Preparations:
-- Create TFT_eSPI User Setup file as described on https://github.com/Xinyuan-LilyGO/TTGO-T-Display/blob/master/README.MD and
-  https://sites.google.com/site/jmaathuis/arduino/lilygo-ttgo-t-display-esp32/arduinomenu-demo-ttgo-t-display
-- Attach a led (and a resistor if needed). This is optional.
-
-This demo can also work on other setups with displays supported by the TFT_eSPI library. This demo is explicitly for the
-TTGO T-display but I had a slightly modifies version working on a Wemos D1 mini with attached ST7735 display and some image
-and text modifications as well another User Setup file for TFT_eSPI.
-
-For a more detailed description about this demo please visit:
-https://sites.google.com/site/jmaathuis/arduino/lilygo-ttgo-t-display-esp32/arduinomenu-demo-ttgo-t-display
-
-This sketch may freely redistributed as long as you mention the sources / authors mentioned above. I would be very happy to here from
-you when you have improved the sketch. You can mail me at j [dot] maathuis [at] gmail [dot] com.
-
-****************************************************************************************************************************************/
-#define PROG_VERSION "0.2" // Don't change this!
-
-//#include <SPI.h>
-#include <TFT_eSPI.h>
-#include <Button2.h>
-#include "bmp.h"
-
-#ifndef TFT_DISPOFF
-#define TFT_DISPOFF 0x28
-#endif
-
-#ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
-#endif
-
-#define TFT_BL          4  // Display backlight control pin
-
-#define LED_CTRL        0 // Pinnumber of external led to control. Set to 0 if there isn't a external led attached.
-
-int ledBacklight = 80; // Initial TFT backlight intensity on a scale of 0 to 255. Initial value is 80.
-
-#define BTN_UP 35 // Pinnumber for button for up/previous and select / enter actions (don't change this if you want to use the onboard buttons)
-#define BTN_DWN 0 // Pinnumber for button for down/next and back / exit actions (don't change this if you want to use the onboard buttons)
-
+// #include <TFT_HX8357.h> // included by menu driver <menuIO/TFT_HX8357Out.h>
 #include <menu.h>
+#include "TFT_FeatherWing.h"
+#define USE_CLICK_ENCODER
+#ifdef USE_CLICK_ENCODER
+  //#include <TimerOne.h>
+  //#include <ClickEncoder.h>
+  //#include <menuIO/clickEncoderIn.h>
+#else
+  #include <menuIO/encoderIn.h>
+#endif
+#include <menuIO/keyIn.h>
+#include <menuIO/chainStream.h>
 #include <menuIO/serialIO.h>
-#include <menuIO/TFT_eSPIOut.h>
-// #include <menuIO/chainStream.h>
-#include <menuIO/esp8266Out.h>//must include this even if not doing web output...
 
 using namespace Menu;
 
-TFT_eSPI gfx = TFT_eSPI(); // Define TFT_eSPI object with the size of the screen: 135 pixels width and 240 pixels height. We will rotate it later a quarter clockwise.
+#define fontW 16
+#define fontH 16
 
-Button2 btnUp(BTN_UP); // Initialize the up button
-Button2 btnDwn(BTN_DWN); // Initialize the down button
+#define STMPE_CS 32
+#define TFT_CS   15
+#define TFT_DC   33
+#define SD_CS    14
+
+#define TFT_RST -1
+
+Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);  // Invoke custom library
+
+#define LEDPIN LED_BUILTIN
+
+// rotary encoder pins
+#define encA    2
+#define encB    3
+#define encBtn  4
 
 result doAlert(eventMask e, prompt &item);
 
 int test=55;
 
-int ledCtrl=LOW; // Initial value for external connected led
-
-// Setting PWM properties, do not change this!
-const int pwmFreq = 5000;
-const int pwmResolution = 8;
-const int pwmLedChannelTFT = 0;
+int ledCtrl=LOW;
 
 result myLedOn() {
   ledCtrl=HIGH;
@@ -125,8 +107,8 @@ MENU(subMenu,"Sub-Menu",doNothing,noEvent,noStyle
   ,EXIT("<Back")
 );
 
-const char* constMEM hexDigit MEMMODE="0123456789ABCDEF";
-const char* constMEM hexNr[] MEMMODE={"0","x",hexDigit,hexDigit};
+char* constMEM hexDigit MEMMODE="0123456789ABCDEF";
+char* constMEM hexNr[] MEMMODE={"0","x",hexDigit,hexDigit};
 char buf1[]="0x11";
 
 MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
@@ -137,7 +119,6 @@ MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
   ,SUBMENU(setLed)
   ,OP("LED On",myLedOn,enterEvent)
   ,OP("LED Off",myLedOff,enterEvent)
-  ,FIELD(ledBacklight,"Backlight: ","",0,255,10,5,doNothing,noEvent,wrapStyle) // Menu option to set the intensity of the backlight of the screen.
   ,SUBMENU(selMenu)
   ,SUBMENU(chooseMenu)
   //,OP("Alert test",doAlert,enterEvent)
@@ -149,252 +130,107 @@ MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
 //  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
 //monochromatic color table
 
-
-#define Black RGB565(0,0,0)
-#define Red	RGB565(255,0,0)
-#define Green RGB565(0,255,0)
-#define Blue RGB565(0,0,255)
-#define Gray RGB565(128,128,128)
-#define LighterRed RGB565(255,150,150)
-#define LighterGreen RGB565(150,255,150)
-#define LighterBlue RGB565(150,150,255)
-#define DarkerRed RGB565(150,0,0)
-#define DarkerGreen RGB565(0,150,0)
-#define DarkerBlue RGB565(0,0,150)
-#define Cyan RGB565(0,255,255)
-#define Magenta RGB565(255,0,255)
-#define Yellow RGB565(255,255,0)
-#define White RGB565(255,255,255)
+#define TFT_GREY 0x5AEB
 
 const colorDef<uint16_t> colors[6] MEMMODE={
-  {
-    {
-      (uint16_t)Black,
-      (uint16_t)Black
-    },
-    {
-      (uint16_t)Black,
-      (uint16_t)DarkerBlue,
-      (uint16_t)DarkerBlue
-    }
-  },//bgColor
-  {
-    {
-      (uint16_t)Gray,
-      (uint16_t)Gray
-    },
-    {
-      (uint16_t)White,
-      (uint16_t)White,
-      (uint16_t)White
-    }
-  },//fgColor
-  {
-    {
-      (uint16_t)White,
-      (uint16_t)Black
-    },
-    {
-      (uint16_t)Yellow,
-      (uint16_t)Yellow,
-      (uint16_t)Red
-    }
-  },//valColor
-  {
-    {
-      (uint16_t)White,
-      (uint16_t)Black
-    },
-    {
-      (uint16_t)White,
-      (uint16_t)Yellow,
-      (uint16_t)Yellow
-    }
-  },//unitColor
-  {
-    {
-      (uint16_t)White,
-      (uint16_t)Gray
-    },
-    {
-      (uint16_t)Black,
-      (uint16_t)Blue,
-      (uint16_t)White
-    }
-  },//cursorColor
-  {
-    {
-      (uint16_t)White,
-      (uint16_t)Yellow
-    },
-    {
-      (uint16_t)DarkerRed,
-      (uint16_t)White,
-      (uint16_t)White
-    }
-  },//titleColor
+  {{HX8357_BLACK,HX8357_BLACK},{HX8357_BLACK,HX8357_BLUE,HX8357_BLUE}},//bgColor
+  {{HX8357_MAGENTA,HX8357_MAGENTA},{HX8357_WHITE,HX8357_WHITE,HX8357_WHITE}},//fgColor
+  {{HX8357_WHITE,HX8357_BLACK},{HX8357_YELLOW,HX8357_YELLOW,HX8357_RED}},//valColor
+  {{HX8357_WHITE,HX8357_BLACK},{HX8357_WHITE,HX8357_YELLOW,HX8357_YELLOW}},//unitColor
+  {{HX8357_WHITE,HX8357_MAGENTA},{HX8357_BLACK,HX8357_BLUE,HX8357_WHITE}},//cursorColor
+  {{HX8357_WHITE,HX8357_YELLOW},{HX8357_BLUE,HX8357_RED,HX8357_RED}},//titleColor
 };
-
-#define MAX_DEPTH 5
 
 serialIn serial(Serial);
 
-//MENU_INPUTS(in,&serial);its single, no need to `chainStream`
+#ifdef USE_CLICK_ENCODER
+  // ClickEncoder clickEncoder(encA,encB,encBtn);
+  // ClickEncoderStream encStream(clickEncoder,1);
+  // MENU_INPUTS(in,&encStream,&serial);
+  // void timerIsr() {clickEncoder.service();}
+  MENU_INPUTS(in,&serial);
+#else
+  encoderIn<encA,encB> encoder;//simple quad encoder driver
+  encoderInStream<encA,encB> encStream(encoder,4);// simple quad encoder fake Stream
+  //a keyboard with only one key as the encoder button
+  keyMap encBtn_map[]={{-encBtn,defaultNavCodes[enterCmd].ch}};//negative pin numbers use internal pull-up, this is on when low
+  keyIn<1> encButton(encBtn_map);//1 is the number of keys
+  MENU_INPUTS(in,&encStream,&encButton,&serial);
+#endif
 
-//define serial output device
+#define MAX_DEPTH 4
+
+//define output device
 idx_t serialTops[MAX_DEPTH]={0};
 serialOut outSerial(Serial,serialTops);
 
-#define GFX_WIDTH 240
-#define GFX_HEIGHT 135
-#define fontW 12
-#define fontH 18
+const panel panels[] MEMMODE={{0,0,480/fontW,320/fontH}};
+navNode* nodes[sizeof(panels)/sizeof(panel)];//navNodes to store navigation status
+panelsList pList(panels,nodes,1);//a list of panels and nodes
+idx_t gfxTops[MAX_DEPTH]={0};
+tftFeatherWingOut tftOut(tft,colors,gfxTops,pList,fontW,fontH);
 
-const panel panels[] MEMMODE = {{0, 0, GFX_WIDTH / fontW, GFX_HEIGHT / fontH}};
-navNode* nodes[sizeof(panels) / sizeof(panel)]; //navNodes to store navigation status
-panelsList pList(panels, nodes, 1); //a list of panels and nodes
-idx_t eSpiTops[MAX_DEPTH]={0};
-TFT_eSPIOut eSpiOut(gfx,colors,eSpiTops,pList,fontW,fontH+1);
-menuOut* constMEM outputs[] MEMMODE={&outSerial,&eSpiOut};//list of output devices
-outputsList out(outputs,sizeof(outputs)/sizeof(menuOut*));//outputs list controller
+//define outputs controller
+menuOut* constMEM outputs[] MEMMODE={&outSerial,&tftOut};//list of output devices
+outputsList out(outputs,2);//outputs list controller
 
-NAVROOT(nav,mainMenu,MAX_DEPTH,serial,out);
+//define navigation root and aux objects
+navNode nav_cursors[MAX_DEPTH];//aux objects to control each level of navigation
+navRoot nav(mainMenu, nav_cursors, MAX_DEPTH, in, out);
 
-unsigned long idleTimestamp = millis();
+// MENU_OUTPUTS(out,MAX_DEPTH
+//   ,ADAGFX_OUT(tft,colors,6*textScale,9*textScale,{0,0,14,8},{14,0,14,8})
+//   ,SERIAL_OUT(Serial)
+// );
+//
+// NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
 
 //when menu is suspended
 result idle(menuOut& o,idleEvent e) {
   if (e==idling) {
-      // Show the idle message once
-      int xpos = gfx.width() / 2; // Half the screen width
-      gfx.fillScreen(Black);
-
-      gfx.setTextSize(5);
-      gfx.setTextColor(Yellow,Black);
-      gfx.setTextWrap(false);
-      gfx.setTextDatum(MC_DATUM);
-      gfx.drawString("IDLE", xpos, 50);
-      int getFontHeight = gfx.fontHeight();
-
-      gfx.setTextSize(2);
-      gfx.setTextColor(White,Black);
-      gfx.setTextDatum(MC_DATUM);
-      gfx.drawString("Long press a button", xpos, 90);
-      gfx.drawString("to exit", xpos, 110);
+    o.println(F("suspended..."));
+    o.println(F("press [select]"));
+    o.println(F("to continue"));
   }
   return proceed;
 }
 
 //config myOptions('*','-',defaultNavCodes,false);
 
-void button_init()
-{
-    btnUp.setLongClickHandler([](Button2 & b) {
-        // Select
-        unsigned int time = b.wasPressedFor();
-        if (time >= 1000) {
-          nav.doNav(enterCmd);
-        }
-    });
-
-    btnUp.setClickHandler([](Button2 & b) {
-       // Up
-       nav.doNav(downCmd); // It's called downCmd because it decreases the index of an array. Visually that would mean the selector goes upwards.
-    });
-
-    btnDwn.setLongClickHandler([](Button2 & b) {
-        // Exit
-        unsigned int time = b.wasPressedFor();
-        if (time >= 1000) {
-          nav.doNav(escCmd);
-        }
-    });
-
-    btnDwn.setClickHandler([](Button2 & b) {
-        // Down
-        nav.doNav(upCmd); // It's called upCmd because it increases the index of an array. Visually that would mean the selector goes downwards.
-    });
-}
-
-void button_loop()
-{
-    // Check for button presses
-    btnUp.loop();
-    btnDwn.loop();
-}
-
 void setup() {
   //options=&myOptions;//can customize options
-  Serial.begin(115200); // Set Serial baudrate at 115200
+  pinMode(LEDPIN,OUTPUT);
+  Serial.begin(115200);
   while(!Serial);
+  Serial.println("menu 4.x test");
   Serial.flush();
-  Serial.println();
-  Serial.print("ARDUINOMENU DEMO V");
-  Serial.print(PROG_VERSION);
-  Serial.println(" FOR LILYGO TTGO T-DISPLAY");
-
   nav.idleTask=idle;//point a function to be used when menu is suspended
   mainMenu[1].disable();
   //outGfx.usePreview=true;//reserve one panel for preview?
   //nav.showTitle=false;//show menu title?
 
-  //SPI.begin(); // Leave this commented or else there will be nothing shown on the screen.
-  gfx.init(); // Initialize the screen.
+  //pinMode(encBtn, INPUT_PULLUP);
+  #ifdef USE_CLICK_ENCODER
+    // Timer1.initialize(1000);
+    // Timer1.attachInterrupt(timerIsr);
+  #else
+    encButton.begin();
+    encoder.begin();
+  #endif
 
-  Serial.print("Configuring PWM for TFT backlight... ");
-  ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
-  ledcAttachPin(TFT_BL, pwmLedChannelTFT);
-  Serial.println("DONE");
-
-  Serial.print("Setting PWM for TFT backlight to default intensity... ");
-  ledcWrite(pwmLedChannelTFT, ledBacklight);
-  Serial.println("DONE");
-
-  gfx.setRotation(1); // Rotate display a quarter clockwise
-
-  gfx.setTextSize(2);
-  gfx.setTextWrap(false);
-  gfx.fillScreen(Black);
-
-  Serial.print("Showing bootlogo... ");
-  gfx.setSwapBytes(true);
-  gfx.pushImage(0, 0,  240, 135, bootlogo);
+  //SPI.begin();
+  tft.begin();
+  tft.setRotation(1);
+  // tft.setTextSize(textScale);//test scalling
+  // tft.setTextWrap(false);
+  tft.fillScreen(HX8357_BLACK);
+  // tft.setTextColor(TFT_RED,TFT_BLACK);
+  tft.println("Menu 4.x test HX8257");
   delay(1000);
-  Serial.println("DONE");
-
-  Serial.print("Initialize buttons... ");
-  button_init();
-  delay(1000);
-  Serial.println("DONE");
-
-  Serial.print("Initialize external led... ");
-  if(LED_CTRL > 0){
-    pinMode(LED_CTRL,OUTPUT);
-    digitalWrite(LED_CTRL,ledCtrl);
-    delay(500);
-    Serial.println("DONE");
-  }
-  else{
-    delay(500);
-    Serial.println("NOT SET UP");
-  }
-
-  delay(2000); // A little bit more delay so that you will be able to see the bootlogo.
-
-  Serial.println("- READY -");
-
-  gfx.fillScreen(Black); // Clear the screen to be ready to draw the menu
 }
 
 void loop() {
-  button_loop();
   nav.poll();//this device only draws when needed
-
-  // External connected led
-  if(LED_CTRL > 0){ // Only set led state when a pinnumber for the external led has been provided.
-    digitalWrite(LED_CTRL,ledCtrl);
-  }
-
-  // Set blacklight intenisty of screen
-  ledcWrite(pwmLedChannelTFT, ledBacklight);
+  digitalWrite(LEDPIN, ledCtrl);
+  delay(100);//simulate a delay when other tasks are done
 }
